75e403aef126650588ffc2ee82541efbe57abf8c
Barry Qin <xiaoyu.qin@ieee.org>
2019-12-29 14:26:04 +1100

    init commit

---
+++ configuration/FromGCS.go
@@ -0,0 +1,76 @@
+package configuration
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+
+	"cloud.google.com/go/storage"
+)
+
+// FromGCS TODO
+func FromGCS(loc string) []byte {
+
+	if !strings.HasPrefix(loc, "gs://") {
+		return nil
+	}
+
+	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
+	if projectID == "" {
+		return nil
+	}
+
+	gsExp := regexp.MustCompile(`gs://(?P<bucket>[^/]+)/(?P<object>.+)`)
+	match := gsExp.FindStringSubmatch(loc)
+
+	var bucket, object string
+
+	for i, name := range gsExp.SubexpNames() {
+		if i != 0 {
+			if name == "bucket" {
+				bucket = match[i]
+				if object != "" {
+					break
+				}
+			} else if name == "object" {
+				object = match[i]
+				if bucket != "" {
+					break
+				}
+			}
+		}
+	}
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data, err := readGCS(client, bucket, object)
+	if err != nil {
+		log.Fatalf("Cannot read object: %v", err)
+	}
+
+	return data
+}
+
+func readGCS(client *storage.Client, bucket, object string) ([]byte, error) {
+	ctx := context.Background()
+	// [START download_file]
+	rc, err := client.Bucket(bucket).Object(object).NewReader(ctx)
+	if err != nil {
+		return nil, err
+	}
+	defer rc.Close()
+
+	data, err := ioutil.ReadAll(rc)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
+	// [END download_file]
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.12
 
 require (
 	cloud.google.com/go/pubsub v1.1.0
+	cloud.google.com/go/storage v1.0.0
 	github.com/Good-Will/middlewares v0.0.0-20191229023924-3c9420cb6147
 )
